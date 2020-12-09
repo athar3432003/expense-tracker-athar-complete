d1e31adc751f41995cd0cd6f3ef1a3dfb26a7881
athar3432003 <75250083+athar3432003@users.noreply.github.com>
2020-12-09 15:37:58 +0500

    server.js

--- server.js
+++ server.js
@@ -23,9 +23,12 @@ app.use("/api/v1/transactions", transactions);
 
 if ((process.env.NODE_ENV = "production")) {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    //res.sendFile(path.join(__dirname,"client/build/index.html"));
+    const index = path.join(__dirname, "index.html");
+    res.sendFile(index);
+  });
 }
 
 //app.get("/", (req, res) => res.send("Hello world"));

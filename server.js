9370698e0cc6fbde12fad5163aa6d55783f44587
athar3432003 <75250083+athar3432003@users.noreply.github.com>
2020-12-09 15:42:26 +0500

    server.js

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ if ((process.env.NODE_ENV = "production")) {
   app.get("*", (req, res) => {
     //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     //res.sendFile(path.join(__dirname,"client/build/index.html"));
+    console.log("working directory", __dirname);
     const index = path.join(__dirname, "index.html");
     res.sendFile(index);
   });
